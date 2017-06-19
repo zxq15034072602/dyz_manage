e8782085ed4aaf4d61703b462d78320fd7ad0d40
fengxin5571 <502251245@qq.com>
2017-06-19 17:32:08 +0800

    2017 6 19 coding

---
+++ action/index/action.sms.php
@@ -0,0 +1,7 @@
+<?php
+/**
+* 独一张管理app短信验证码类
+* @date: 2017年6月19日 下午3:35:02
+* @author: fx
+*/
+if(!defined("CORE")) exit("error");
--- action/member/action.member.php
+++ action/member/action.member.php
@@ -7,4 +7,17 @@
 if(!defined("CORE")) exit("error");
 if($do == "login"){//用户登陆
     $user_type=$_REQUEST['type']??0;//所屬用戶 （0独一张，1食维健）
+    $sql="select * from rv_user where 1=1 and username=? and password=? and type=? and status=1";
+    $db->p_e($sql,array($_POST['user_name'],md5($_POST['password']),$user_type));
+    $user=$db->fetchRow();
+    if($user['id']>0){
+        $sql="select * from rv_role where 1=1 and  id=?";
+        $db->p_e($sql,array());
+        echo '{"code":"200","uid":"'.$user['id'].'","roleid":"'.$user[roleid].'"}';//登陆成功返回code：200 用户id 与角色权限id
+    }else{
+        echo '{"code":"500","msg":"登陆信息有误"}';
+    }
+    exit;
+}elseif($do == "register"){//用户注册
+    
 }
